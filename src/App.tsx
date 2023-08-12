ec45f0ac7aff65d618106ac7e7a8ffd89bb72787
suhyun ji <wldmx3@gmail.com>
2023-08-12 15:33:24 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import MockSamples from "./MockSamples"
+
+function App() {
+  return (
+    <div className="w-full border border-black m-3 p-1">
+      <main>
+        <div>페이히어 Frontend 과제입니다.</div>
+        <MockSamples />
+      </main>
+    </div>
+  )
+}
+
+export default App
