8f0d4c07ce7bc0af0a30945191b625b10837e830
suhyun ji <wldmx3@gmail.com>
2023-08-13 21:46:22 +0900

    Feat : 배너 구현 중

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -2,19 +2,18 @@ import React from "react";
 import { SiCoffeescript } from "react-icons/si";
 
 interface CategoryProps {
-    label: string;
+  label: string;
 }
 
-const Category: React.FC<CategoryProps> = ({ label }) => {
-    return (
-        <li className="p-3 border-2 rounded-xl my-3 cursor-pointer">
-            <div className="flex-initial items-center justify-center text-3xl">
-                <SiCoffeescript />
-                <h3>{label}</h3>
-            </div>
-        </li>
-    );
-    
+const Category: React.FC<CategoryProps> = ({ label}) => {
+  return (
+    <li className="p-3 border-2 rounded-xl my-3 cursor-pointer flex items-center justify-center ">
+      <div className="text-center">
+        <SiCoffeescript className="sm:text-xl md:text-2xl lg:text-5xl" />
+        <h3 className="sm:text-xs md:text-sm lg:text-base">{label}</h3>
+      </div>
+    </li>
+  );
 };
 
 export default Category;
