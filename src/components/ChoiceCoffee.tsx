8f02952f9146f7cefe95acb49afe820640289b8e
suhyun ji <wldmx3@gmail.com>
2023-08-16 00:04:25 +0900

    Feat: 카트 창에 선택 목록 요소들 적용 완료(추가 구현 필요)

--- src/components/ChoiceCoffee.tsx
+++ src/components/ChoiceCoffee.tsx
@@ -1,21 +1,34 @@
 import { PiCoffeeDuotone, PiXBold } from "react-icons/pi";
 import { ICartItem } from "../commonTypes";
-export default function ChoiceCoffee({selectedItem} : {selectedItem : ICartItem}) {
-
+// 수량 증가 감소 하면서 각 항목의 총 합계 작성 필요
+// total은 selector로 구현
+// pay에서는 quantityButoon이 필요 없고 여기서는 필요 따라서 이거 구별해서 props 전달
+export default function ChoiceCoffee({ selectedItem }: { selectedItem: ICartItem }) {
+  console.log(selectedItem);
   return (
     <div className="flex justify-between my-6 border-b-2">
-      <PiCoffeeDuotone className="text-4xl ml-2" />
-      <div className="mx-3 text-right ">
+      <div className="mx-3 text-right">
         <div className="text-gray-600 mb-1 sm:text-sm lg:text-xl">
           {selectedItem.name}
         </div>
-        <div className="text-sm text-gray-400 mb-2">Just Now</div>
+
+        {selectedItem.option?.map((op) => (
+          <div className="flex justify-end text-sm text-gray-400 mb-2" key={op.name}>
+            <div>{op.name}</div>
+            {op.price && (
+              <div>{`${op.price * selectedItem.quantity}원`}</div>
+            )}
+          </div>
+        ))}
         {/* <QuantityButton /> */}
       </div>
-      <button className="flex justify-start mr-4 mt-0 rounded-md text-gray-400 hover:text-gray-700">
+      <div className="text-gray-600 mb-1 sm:text-sm lg:text-xl">
+        {selectedItem.price * selectedItem.quantity}원
+      </div>
+      <button className="flex justify-end mr-4 mt-0 rounded-md text-gray-400 hover:text-gray-700">
         <PiXBold />
       </button>
-    </div>
+    </div >
   );
 }
 
--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 import { ICartItem } from '../commonTypes';
 
 
@@ -6,3 +6,25 @@ export const cartState = atom<ICartItem[]>({
   key: 'cartState',
   default: []
 });
+
+// export const totalPriceSelector = selector({
+//   key: 'totalPriceSelector',
+//   get: ({ get }) => {
+//     const cart = get(cartState);
+//     const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+//     return total;
+//   }
+// });
+
+// import { useRecoilValue } from 'recoil';
+// import { totalPriceSelector } from './selectors'; // 위에서 정의한 selector
+
+// const CartTotal = () => {
+//   const totalPrice = useRecoilValue(totalPriceSelector);
+
+//   return (
+//     <div>
+//       <p>Total Price: {totalPrice}원</p>
+//     </div>
+//   );
+// };
