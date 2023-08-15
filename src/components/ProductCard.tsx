164b42e11c1b9dcb62f78534c1910f29d87e8967
suhyun ji <wldmx3@gmail.com>
2023-08-15 13:58:55 +0900

    Fix: 모달 오류

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import image from '../assets/image/coffee.jpg';
 import { IProduct, IOption } from "../commonTypes";
 
 // Product Card
-export default function ProductCard({ product }: {product : IProduct}) {
+export default function ProductCard({ product }: { product: IProduct }) {
     const [openOptionModal, setOpenOptionModal] = useState<boolean>(false);
 
     const handleImgError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
@@ -12,7 +12,11 @@ export default function ProductCard({ product }: {product : IProduct}) {
     };
 
     const handleOpenModal = () => {
-        setOpenOptionModal(!openOptionModal);
+        setOpenOptionModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenOptionModal(false);
     };
 
     return (
@@ -31,7 +35,7 @@ export default function ProductCard({ product }: {product : IProduct}) {
                 {/* Option Modal */}
                 {product.option ? (
                     openOptionModal && (
-                        <ProductOption options={product.option} product={product} />
+                        <ProductOption options={product.option} product={product} handleModal={handleCloseModal} />
                     )
                 ) : <></>}
 
@@ -41,7 +45,10 @@ export default function ProductCard({ product }: {product : IProduct}) {
 }
 
 // Option Modal
-function ProductOption({ options, product}: { options: IOption[], product : IProduct}) {
+function ProductOption({ options, product, handleModal }: { options: IOption[], product: IProduct, handleModal: () => void }) {
+    const handleCloseModal = () => {
+        handleModal();
+    };
     return (
         <div className="relative z-10" role="dialog" aria-modal="true">
             <div className="fixed inset-0 hidden bg-gray-500 bg-opacity-75 transition-opacity md:block"></div>
@@ -49,12 +56,6 @@ function ProductOption({ options, product}: { options: IOption[], product : IPro
                 <div className="flex min-h-full items-stretch justify-center text-center md:items-center md:px-2 lg:px-4">
                     <div className="flex transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
                         <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
-                            <button type="button" className="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8">
-                                <span className="sr-only">Close</span>
-                                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </button>
 
                             <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-10 lg:gap-x-8">
                                 <div className="sm:col-span-8 lg:col-span-7">
@@ -79,7 +80,7 @@ function ProductOption({ options, product}: { options: IOption[], product : IPro
                                     <section aria-labelledby="options-heading" className="mt-10">
                                         <h3 id="options-heading" className="sr-only">Product options</h3>
 
-                                        <form>
+                                        <div>
                                             <div>
                                                 <h4 className="text-sm font-medium text-gray-900">Color</h4>
                                                 <div className="h-8">
@@ -100,14 +101,27 @@ function ProductOption({ options, product}: { options: IOption[], product : IPro
 
                                                             <span className="pointer-events-none absolute -inset-px rounded-md" aria-hidden="true"></span>
                                                         </label>
+                                                        <label className="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 cursor-pointer bg-white text-gray-900 shadow-sm">
+                                                            <input type="radio" name="size-choice" value="XXS" className="sr-only" aria-labelledby="size-choice-0-label" />
+                                                            <span id="size-choice-0-label">XXS</span>
+
+                                                            <span className="pointer-events-none absolute -inset-px rounded-md" aria-hidden="true"></span>
+                                                        </label>
+                                                        <label className="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 cursor-pointer bg-white text-gray-900 shadow-sm">
+                                                            <input type="radio" name="size-choice" value="XXS" className="sr-only" aria-labelledby="size-choice-0-label" />
+                                                            <span id="size-choice-0-label">XXS</span>
+
+                                                            <span className="pointer-events-none absolute -inset-px rounded-md" aria-hidden="true"></span>
+                                                        </label>
                                                         <div className="h-8">
 
                                                         </div>
                                                     </div>
                                                 </fieldset>
                                             </div>
-                                            <button type="submit" className="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Add to bag</button>
-                                        </form>
+                                            <button onClick={handleCloseModal} className="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">취소</button>
+                                            <button type="submit" className="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">선택</button>
+                                        </div>
                                     </section>
                                 </div>
                             </div>
