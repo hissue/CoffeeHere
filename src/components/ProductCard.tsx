1efba844c4a6717d9f6910a8c98a00b3a114c59e
suhyun ji <wldmx3@gmail.com>
2023-08-17 00:56:13 +0900

    Fix : Product 모달 이미지 삭제

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -97,22 +97,16 @@ function ProductOption({ options, product, handleModal }: { options: IOption[], 
         setCart([...cart, newProduct]);
     };
 
-    console.log(cart);
-
     return (
         <div className="relative z-10" role="dialog" aria-modal="true">
             <div className="fixed inset-0 hidden bg-gray-500 bg-opacity-75 transition-opacity md:block"></div>
             <div className="fixed inset-0 z-10 overflow-y-auto">
                 <div className="flex min-h-full items-stretch justify-center text-center md:items-center md:px-2 lg:px-4">
 
-                    <div className="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
+                    <div className="flex w-full transform text-left text-base transition md:my-8 md:max-w-xl md:px-4 lg:max-w-2xl">
                         <div className="relative flex w-full items-center rounded-2xl overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
-                            <div className="justify-between grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
-                                <div className="sm:col-span-4 lg:col-span-5">
-                                    <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100">
-                                        <img src="https://tailwindui.com/img/ecommerce-images/product-quick-preview-03-detail.jpg" alt="Interior of light green canvas bag with padded laptop sleeve and internal organization pouch." className="object-cover object-center" />
-                                    </div>
-                                </div>
+                            <div className="justify-between grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-3 lg:gap-x-8">
+
                                 <div className="sm:col-span-8 lg:col-span-7">
                                     <h2 className="text-3xl font-bold text-gray-900 sm:pr-12">{product.name}</h2>
 
